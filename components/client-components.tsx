9fbed7794f074493907264fc187fcc3fb4cb3019
Rifat Khan <rifat6633@gmail.com>
2024-03-17 16:10:34 +0600

    update: ModeToggle

--- components/client-components.tsx
+++ components/client-components.tsx
@@ -2,7 +2,7 @@
 
 import { useTheme } from "next-themes";
 import { Moon, Sun } from "lucide-react";
-import { Button } from "./ui/button";
+import { Button } from "@/components/ui/button";
 
 export function ModeToggle() {
   const { theme, setTheme } = useTheme();
@@ -17,7 +17,9 @@ export function ModeToggle() {
 
   return (
     <Button variant={"outline"} size={"icon"} onClick={() => handleClick()}>
-      {theme === "dark" ? <Sun /> : <Moon />}
+      <Sun className="h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+      <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+      <span className="sr-only">Toggle theme</span>
     </Button>
   );
 }
