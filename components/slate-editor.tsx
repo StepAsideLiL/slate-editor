b1961bc9dd61a119ac60b67782702e4e3172e97a
Rifat Khan <rifat6633@gmail.com>
2024-03-17 16:36:17 +0600

    create: initial SlateEditor

---
+++ components/slate-editor.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import React, { useState } from "react";
+import { createEditor, BaseEditor, Descendant } from "slate";
+
+import { Slate, Editable, withReact, ReactEditor } from "slate-react";
+
+type CustomElement = { type: "paragraph"; children: CustomText[] };
+type CustomText = { text: string };
+
+declare module "slate" {
+  interface CustomTypes {
+    Editor: BaseEditor & ReactEditor;
+    Element: CustomElement;
+    Text: CustomText;
+  }
+}
+
+const initialValue: Descendant[] = [
+  {
+    type: "paragraph",
+    children: [{ text: "A line of text in a paragraph." }],
+  },
+];
+
+export default function SlateEditor() {
+  const [editor] = useState(() => withReact(createEditor()));
+
+  return (
+    <Slate editor={editor} initialValue={initialValue}>
+      <Editable className="focus-visible:outline-none" />
+    </Slate>
+  );
+}
