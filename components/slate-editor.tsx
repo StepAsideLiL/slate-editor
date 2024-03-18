6941a7cbf7aae85e3c762899707d1f98591c77b0
Rifat Khan <rifat6633@gmail.com>
2024-03-18 11:00:12 +0600

    add: code element

--- components/slate-editor.tsx
+++ components/slate-editor.tsx
@@ -1,11 +1,24 @@
 "use client";
 
-import React, { useState } from "react";
-import { createEditor, BaseEditor, Descendant } from "slate";
+import React, { useCallback, useState } from "react";
+import {
+  createEditor,
+  BaseEditor,
+  Descendant,
+  Transforms,
+  Editor,
+  Element,
+} from "slate";
 
-import { Slate, Editable, withReact, ReactEditor } from "slate-react";
+import {
+  Slate,
+  Editable,
+  withReact,
+  ReactEditor,
+  RenderElementProps,
+} from "slate-react";
 
-type CustomElement = { type: "paragraph"; children: CustomText[] };
+type CustomElement = { type: "paragraph" | "code"; children: CustomText[] };
 type CustomText = { text: string };
 
 declare module "slate" {
@@ -26,12 +39,51 @@ const initialValue: Descendant[] = [
 export default function SlateEditor() {
   const [editor] = useState(() => withReact(createEditor()));
 
+  const renderElement = useCallback((props: RenderElementProps) => {
+    switch (props.element.type) {
+      case "code":
+        return <CodeElement {...props} />;
+      default:
+        return <DefaultElement {...props} />;
+    }
+  }, []);
+
   return (
     <Slate editor={editor} initialValue={initialValue}>
       <Editable
         className="focus-visible:outline-none"
         placeholder="Write something"
+        renderElement={renderElement}
+        onKeyDown={(event) => {
+          if (event.key === "`" && event.ctrlKey) {
+            // Prevent the "`" from being inserted by default.
+            event.preventDefault();
+            const [match] = Editor.nodes(editor, {
+              match: (n: any) => n.type === "code",
+            });
+            // Otherwise, set the currently selected blocks type to "code".
+            Transforms.setNodes(
+              editor,
+              { type: match ? "paragraph" : "code" },
+              {
+                match: (n) => Element.isElement(n) && Editor.isBlock(editor, n),
+              },
+            );
+          }
+        }}
       />
     </Slate>
   );
 }
+
+const CodeElement = (props: RenderElementProps) => {
+  return (
+    <pre {...props.attributes}>
+      <code>{props.children}</code>
+    </pre>
+  );
+};
+
+const DefaultElement = (props: RenderElementProps) => {
+  return <p {...props.attributes}>{props.children}</p>;
+};
