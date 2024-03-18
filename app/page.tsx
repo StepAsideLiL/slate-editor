711b21507cabc6c3d31ba4542b0635ca4aeeb25f
Rifat Khan <rifat6633@gmail.com>
2024-03-18 11:53:38 +0600

    add: EditorV2

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,41 @@ import EditorV1 from "@/components/editors/editor-v1";
 
 export default function Page() {
   return (
-    <main className="container">
+    <main className="container space-y-10">
       {/* <SlateEditor /> */}
-      <EditorV1 />
+      <section className="space-y-3 px-10">
+        <div className="text-muted-foreground">
+          <h1 className="text-xl">Editor 1</h1>
+          <p>
+            Toggle between code block and paragraph with{" "}
+            <span className="font-mono">
+              <code className="rounded bg-muted px-1 py-[1px]">ctrl</code> +{" "}
+              <code className="rounded bg-muted px-1 py-[1px]">`</code>
+            </span>
+          </p>
+        </div>
+
+        <div className="min-h-20 border border-muted p-3">
+          <EditorV1 />
+        </div>
+      </section>
+
+      <section className="space-y-3 px-10">
+        <div className="text-muted-foreground">
+          <h1 className="text-xl">Editor 1</h1>
+          <p>
+            Toggle between code block and paragraph with{" "}
+            <span className="font-mono">
+              <code className="rounded bg-muted px-1 py-[1px]">ctrl</code> +{" "}
+              <code className="rounded bg-muted px-1 py-[1px]">`</code>
+            </span>
+          </p>
+        </div>
+
+        <div className="min-h-20 border border-muted p-3">
+          <EditorV1 />
+        </div>
+      </section>
     </main>
   );
 }
