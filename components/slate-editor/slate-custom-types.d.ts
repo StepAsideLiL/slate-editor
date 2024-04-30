1f616ced1d321e403200ff3a58928e08379ec324
Rifat Khan <rifat6633@gmail.com>
2024-04-30 21:38:41 +0600

    create: types definition for slate

---
+++ components/slate-editor/slate-custom-types.d.ts
@@ -0,0 +1,28 @@
+export type CustomElement = { type: 'paragraph' | "h1" | "h2" | "h3"; children: CustomText[] }
+export type CustomText = { text: string }
+
+export declare module 'slate' {
+  interface CustomTypes {
+    Editor: BaseEditor & ReactEditor
+    Element: CustomElement
+    Text: CustomText
+  }
+}
+
+export type H1Element = {
+  type: 'h1'
+  character: string
+  children: CustomText[]
+}
+
+export type H2Element = {
+  type: 'h2'
+  character: string
+  children: CustomText[]
+}
+
+export type H3Element = {
+  type: 'h3'
+  character: string
+  children: CustomText[]
+}
