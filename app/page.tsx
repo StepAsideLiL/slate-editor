0c6831a022489412beebb6a8c466b9f35e392d96
Rifat Khan <rifat6633@gmail.com>
2024-04-30 21:37:50 +0600

    add: new SlateEditor component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import SlateEditor from "@/components/slate-editor/slate-editor";
+
 export default function Page() {
   return (
     <main className="container space-y-10 pb-20">
-      Hello
+      <SlateEditor />
     </main>
   );
 }
