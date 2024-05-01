aa33d9dd164bf70d145538c1b8955f1de36a77b0
Rifat Khan <rifat6633@gmail.com>
2024-05-01 10:59:17 +0600

    refactor: apply prettier

--- components/slate-editor/slate-custom-types.d.ts
+++ components/slate-editor/slate-custom-types.d.ts
@@ -1,28 +1,31 @@
-export type CustomElement = { type: 'paragraph' | "h1" | "h2" | "h3"; children: CustomText[] }
-export type CustomText = { text: string }
+export type CustomElement = {
+  type: "paragraph" | "h1" | "h2" | "h3";
+  children: CustomText[];
+};
+export type CustomText = { text: string };
 
-export declare module 'slate' {
+export declare module "slate" {
   interface CustomTypes {
-    Editor: BaseEditor & ReactEditor
-    Element: CustomElement
-    Text: CustomText
+    Editor: BaseEditor & ReactEditor;
+    Element: CustomElement;
+    Text: CustomText;
   }
 }
 
 export type H1Element = {
-  type: 'h1'
-  character: string
-  children: CustomText[]
-}
+  type: "h1";
+  character: string;
+  children: CustomText[];
+};
 
 export type H2Element = {
-  type: 'h2'
-  character: string
-  children: CustomText[]
-}
+  type: "h2";
+  character: string;
+  children: CustomText[];
+};
 
 export type H3Element = {
-  type: 'h3'
-  character: string
-  children: CustomText[]
-}
+  type: "h3";
+  character: string;
+  children: CustomText[];
+};
